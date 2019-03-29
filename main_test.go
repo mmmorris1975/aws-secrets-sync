8b2c5ab6bd559770591429fea5b4a1c6ed5910b6
Mike Morris <michael.m.morris@pearson.com>
2019-03-28 23:22:38 -0500

    stub out main module unit tests

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestCheckBoolEnv(t *testing.T) {
+
+}
+
+func TestValidateBackend(t *testing.T) {
+
+}
+
+func TestValidateKey(t *testing.T) {
+
+}
+
+func TestBackendFactory(t *testing.T) {
+
+}
+
+func TestReadBinary(t *testing.T) {
+
+}
+
+func TestGetReader(t *testing.T) {
+
+}

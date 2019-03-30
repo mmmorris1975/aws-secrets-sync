8791177afd41a899767834126f9473fd9d4f6991
Mike Morris <michael.m.morris@pearson.com>
2019-03-30 09:25:41 -0500

    add secretsmanager tests

--- spec/arg_spec.rb
+++ spec/arg_spec.rb
@@ -140,8 +140,37 @@ describe 'tests using command line args' do
     end
   end
 
-  if ENV.fetch("CIRCLECI", false).to_s === "true"; then
-    describe 'for the secrets manager backend' do
+  describe 'for the secrets manager backend' do
+    if ENV.fetch("CIRCLECI", false).to_s === "false"; then
+      describe command ('aws-secrets-sync -s secretsmanager H4sIALF6n1wAA6tWSs4sSs5JTc7UT6/KLFCyUlAqSS0uUarlAgBdWbyoGgAAAA==') do
+        its(:exit_status) { should eq 0 }
+        its(:stderr) { should match /INFO updated secret circleci\/gzip$/ }
+      end
+
+      describe command ('aws-secrets-sync -s secretsmanager eyJjaXJjbGVjaS9iYXNlNjQiOiAidmFsdWUifQo=') do
+        its(:exit_status) { should eq 0 }
+        its(:stderr) { should match /INFO updated secret circleci\/base64/ }
+      end
+
+      describe command ('aws-secrets-sync -s secretsmanager \'{"circleci/text": "item"}\'') do
+        its(:exit_status) { should eq 0 }
+        its(:stderr) { should match /INFO updated secret circleci\/text/ }
+      end
+
+      describe command ('aws-secrets-sync -s secretsmanager -o circleci/one-shot boom') do
+        its(:exit_status) { should eq 0 }
+        its(:stderr) { should match /INFO updated secret circleci\/one-shot/ }
+      end
+    end
+
+    describe command ('aws-secrets-sync -s secretsmanager H4sIAH7PnlwAA6tW0k9KTFGyUlDKKs7PU+ICADjxdc8QAAAA') do
+      its(:exit_status) { should eq 1 }
+      its(:stderr) { should match /ERROR error decoding json: / }
+    end
+
+    describe command ('aws-secrets-sync -s ssm eyIvbm9hY2Nlc3MvYjY0IjogInRlc3QifQo=') do
+      its(:exit_status) { should eq 1 }
+      its(:stderr) { should match /ERROR error storing secret: AccessDeniedException: / }
     end
   end
 end
